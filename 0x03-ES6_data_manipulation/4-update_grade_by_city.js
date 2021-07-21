0cdee4016d5cbcc113c9e19f8ecc99823f412203
= <=>
2021-07-22 01:12:53 +0200

    submit tasks

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,9 @@
 /* eslint-disable */
 export default function updateStudentGradeByCity(students, city, newGrade) {
-    const newArray =  students.map((student) => {
+    return students.map((student) => {
         const studentGrade = newGrade.filter((element) => ( element.studentId === student.id ))
                                      .map((x) => x.grade)[0];
         student["grade"] = studentGrade || 'N/A';
         return student; 
-    })
-    return newArray.filter((element) => (element.location === city));
+    }).filter((element) => (element.location === city));
 };
