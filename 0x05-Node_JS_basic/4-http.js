de7a87652625476dceb573d565392a7c370a4c1e
= <=>
2021-09-07 19:40:06 +0200

    create a server with http

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,15 @@
+/* eslint linebreak-style: ["error", "windows"] */
+const http = require('http');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port, () => {
+
+});
+
+module.exports = app;
