772f326342cfed5c5bacbd84956729ff74d4702f
= <=>
2021-09-07 17:55:12 +0200

    use console instead of process

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 function displayMessage(text) {
-    process.stdout.write(`${text}\n`)
+    // process.stdout.write(`${text}\n`)
+    console.log(text);
 };
 
 module.exports = displayMessage;
