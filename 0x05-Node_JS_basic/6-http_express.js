6c22e578fd43dc6ccb36f5cd8223375278557ddd
= <=>
2021-09-07 18:20:01 +0200

    fix lint

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-const port = 1245
+const port = 1245;
 
 app.listen(port, () => {
-    console.log(`App is ruining on port ${port}`)
+  console.log(`App is ruining on port ${port}`);
 });
